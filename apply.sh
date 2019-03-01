361ebd8865180a60a8da9a34282d4709cbd7447c
Johannes Hamfler <jh@z7k.de>
2019-03-01 12:39:18 +0100

    install zsh plugin

--- apply.sh
+++ apply.sh
@@ -4,8 +4,10 @@ cp .pro* ~
 cp .zsh* ~
 cp .vimrc ~
 cp jh.zsh-theme ~/.oh-my-zsh/themes/jh.zsh-theme
-if [ ! -d ~/.vim/bundle/Vundle.vim ]
-then
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	vim +PluginInstall +qall
+fi
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
